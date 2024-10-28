a302d74d8d6a6a86929e453852e4952a1bcdf554
Chris Wood <96439159+chrisjwood16@users.noreply.github.com>
2024-10-28 12:34:57 +0000

    Correct url

--- notebooks/testing_utils.py
+++ notebooks/testing_utils.py
@@ -351,7 +351,7 @@ def write_monthly_testing_report_html(triggered_tests, passed_tests, testing_fal
         </header>
         <p>This report details testing results for OpenPrescribing measures which have the flag testing_measure = true. Items appearing in the English Prescribing Data for {date} that have not previously appeared in the data (from Jan 2014).</p>
         {jan_alert}
-        <p><a href="https://html-preview.github.io/?url=https://github.com/chrisjwood16/openprescribing_tests/blob/main/reports/list_testing_reports.html">View previous reports</a></p>
+        <p><a href="https://html-preview.github.io/?url=https://github.com/chrisjwood16/openprescribing_tests/blob/main/reports/list_test_reports.html">View previous reports</a></p>
     """
 
     # Check if there are any triggered tests
